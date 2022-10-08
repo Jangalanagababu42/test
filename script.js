b4522b1e2cdf67fbd5f08bfbbebd72d8e523e9f0
Jangala nagababu <60348426+Jangalanagababu42@users.noreply.github.com>
2022-10-08 16:38:56 +0000

    Q6

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const input = document.querySelector('#inp');
+// const input = document.querySelector('#inp');
 const button = document.querySelectorAll('.btn');
 const output = document.querySelector('#output');
 
 function onClickHandler(event) {
-  var click = event.target.value;
+  // var click = event.target.value;
 
 
-  if (click === "red") {
+  // if (click === "red") {
 
-    output.style.color = "red";
-  }
-  else if (click === "green") {
+  //   output.style.color = "red";
+  // }
+  // else if (click === "green") {
 
-    output.style.color = "green";
-  }
-  else {
+  //   output.style.color = "green";
+  // }
+  // else {
 
-    output.style.color = "blue";
-  }
+  //   output.style.color = "blue";
+  // }
+  output.style.display = "none";
 }
 
-function onchangeHandler() {
-  var text = input.value;
-  output.innerText = text;
-}
-input.addEventListener('input', onchangeHandler);
+// function onchangeHandler() {
+//   var text = input.value;
+//   output.innerText = text;
+// }
+// input.addEventListener('input', onchangeHandler);
 
 
 button.forEach(btn => {
